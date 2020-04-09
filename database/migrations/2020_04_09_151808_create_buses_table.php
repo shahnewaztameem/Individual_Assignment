96ce21a0b240b3113cee202fe920d151d468dbb8
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-04-09 21:28:03 +0600

    db migration

---
+++ database/migrations/2020_04_09_151808_create_buses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('bus_name');
+            $table->string('bus_code');
+            $table->integer('operator_id');
+            $table->string('total_seats');
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buses');
+    }
+}
